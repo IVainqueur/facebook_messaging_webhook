bc315d93eff78ef901d2ddecd0ab4cb89b620d7f
IVainqueur <ishimvainqueur@gmail.com>
2022-12-16 15:04:48 +0000

    Update from https://github.com/IVainqueur/facebook-integration/commit/03347c89468656999a62e04b18710fd870422221

--- index.js
+++ index.js
@@ -24,16 +24,22 @@ app.post('/webhook', (req, res) => {
     res.status(200).send("EVENT_RECEIVED");
 })
 app.get('/webhook', (req, res) => {
+    // Parse the query params
     let mode = req.query["hub.mode"];
     let token = req.query["hub.verify_token"];
     let challenge = req.query["hub.challenge"];
-    console.log("[verification]", {
-        mode, token, challenge
-    })
-    if (token === process.env.VerificationToken) {
-        res.status(200).send(challenge)
-    } else {
-        res.status(403).send('You are wrong')
+
+    // Check if a token and mode is in the query string of the request
+    if (mode && token) {
+        // Check the mode and token sent is correct
+        if (mode === "subscribe" && token === process.env.VerificationToken) {
+            // Respond with the challenge token from the request
+            console.log("WEBHOOK_VERIFIED");
+            res.status(200).send(challenge);
+        } else {
+            // Respond with '403 Forbidden' if verify tokens do not match
+            res.sendStatus(403);
+        }
     }
 })
 
